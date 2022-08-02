25def634978c75fad4d349b49462b22c7005cc05
TrungKiennef <109646424+TrungKiennef@users.noreply.github.com>
2022-08-03 00:43:27 +0700

    Create 4.cpp

---
+++ 4.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main(){
+	int a;
+	printf("vui long nhap so a");
+	scanf("%d",&a);
+	int b;
+	printf("vui long nhap so b");
+	scanf("%d",&b);
+	int c;
+	printf("vui long nhap so c");
+	scanf("%d",&c);
+	int MAX = a;
+	if(b > MAX){
+		printf("so lon nhat la b");
+	}else{
+	if(c > MAX){
+		printf("so lon nhat la c");
+	}else{
+		printf("so lon nhat la a");
+}
+	}
+	}
