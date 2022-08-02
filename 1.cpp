de9b8bfdab460792a144121cf7a6d6b37cccd67e
TrungKiennef <109646424+TrungKiennef@users.noreply.github.com>
2022-08-03 00:43:14 +0700

    Create 1.cpp

---
+++ 1.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main(){
+	int a;
+	printf("vui long nhap so a");
+	scanf("%d",&a);
+	int b;
+	printf("vui long nhap so b");
+	scanf("%d",&b);
+	int c;
+	printf("vui long nhap so c");
+	scanf("%d",&c);
+	int MIN = a;
+	if(b < MIN){
+		printf("so nho nhat la b");
+	}else{
+	if(c < MIN){
+		printf("so nho nhat la c");
+	}else{
+		printf("so nho nhat la a");
+}
+	}
+	}
